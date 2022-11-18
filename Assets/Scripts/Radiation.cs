9cc9fabd19ce76080c08498dcf0657b48863d7c2
NoeScientist <chub0105@gmail.com>
2022-11-19 00:50:49 +0300

    Radiance
    For Geldman228

--- Assets/Scripts/Radiation.cs
+++ Assets/Scripts/Radiation.cs
@@ -4,9 +4,31 @@ using UnityEngine;
 
 public class Radiation : MonoBehaviour
 {
+    public GameObject DangerThing;
+    public float Distance, DangerDistance;
 
     private void OnTriggerEnter(Collider other)
     {
-        Debug.Log("Лох");
+        Debug.Log("Игрок вошёл в триггер");
+    }
+    private void OnTriggerStay(Collider other)
+    {
+        if (other.GetComponent<PlayerController>())
+        {
+            DangerThing.transform.LookAt(other.GetComponent<PlayerController>().transform);
+
+        }
+
+    }
+    private void Update()
+    {
+        Ray ray = new Ray(DangerThing.transform.position, DangerThing.transform.forward); ;
+        RaycastHit hit;
+        Debug.DrawRay(DangerThing.transform.position, DangerThing.transform.forward * Distance, Color.green);
+        if (Physics.Raycast(ray, out hit, Distance))
+        {
+            DangerDistance = hit.distance;
+        
+        }
     }
 }
