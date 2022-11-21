7c6eff0a93a4208d940ae3efa7c52e9a7279d92b
Geldman <lolofest10@gmail.com>
2022-11-21 04:00:14 +0300

    New 1

--- Assets/Scripts/HudScript.cs
+++ Assets/Scripts/HudScript.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Globalization;
 using TMPro;
 using UnityEngine;
 using UnityEngine.UI;
@@ -7,12 +8,12 @@ using UnityEngine.UI;
 public class HudScript : MonoBehaviour
 {
     public TextMeshProUGUI barom;
-    public float alt, barometre = 0f, oxygen = 200f, O2Divider;
+    public float alt, barometre, oxygen = 200f, O2Divider;
     public Transform player;
-    public GameObject arrBar, arrO2;
+    public Transform arrBar, arrO2;
     public LayerMask lay;
-    
 
+    public float barRot = 200f / 20f;
     void Update()
     {
         Ray ray = new Ray(player.position, player.up);
@@ -22,18 +23,13 @@ public class HudScript : MonoBehaviour
         if (Physics.Raycast(ray, out hit, 10000f, lay))
         {
             barometre = hit.distance;
-
         }
 
-        barometre = barometre + alt;
-        barom.text = "ְעלמספונ " + Mathf.Floor(barometre);
-        arrBar.transform.rotation = Quaternion.Lerp(arrBar.transform.rotation, Quaternion.Euler(0, 0, barometre), Time.deltaTime);
+        barometre += alt;
+        barom.text = "ְעלמספונ " + Mathf.Round(barometre);
+        arrBar.localRotation = Quaternion.Euler(arrBar.forward * barRot);
 
-        arrO2.transform.rotation = Quaternion.Lerp(arrO2.transform.rotation, Quaternion.Euler(0, 0, -oxygen), Time.deltaTime);
-        while (oxygen <= 0f)
-        {
-            oxygen -= O2Divider;
-        }
+        //arrO2.transform.rotation = ;
 
     }
 }
