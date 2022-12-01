6e1e4a4ec283a022e0ca28c18f14185eaa6680bd
Geldman <lolofest10@gmail.com>
2022-12-01 04:07:52 +0300

    Доделан сигнальный пистолет с анимациями

--- Assets/Scripts/Ammo.cs
+++ Assets/Scripts/Ammo.cs
@@ -2,20 +2,11 @@ using UnityEngine;
 
 public class Ammo : MonoBehaviour
 {
-    private FlayerGun fl;
     public Rigidbody rb;
-    public GameObject ammo;
-
-    void Start()
-    {
-        fl = GetComponent<FlayerGun>();
-
-        rb.AddForce(Vector3.forward * fl.force);
-        Destroy(this);
-    }
 
     private void OnCollisionEnter(Collision collision)
     {
         rb.isKinematic = true;
+        Destroy(this.gameObject, 7f);
     }
 }
--- Assets/Scripts/FlayerGun.cs
+++ Assets/Scripts/FlayerGun.cs
@@ -7,19 +7,55 @@ public class FlayerGun : MonoBehaviour
     public GameObject Bullet;
     public int Ammo;
     [Tooltip("Кол-во патрон после перезарядки")] public int Magazine;
-    public float force;
+    public float force, reloadTime ,shootTime;
     public Transform dulo;
+    private Animator anim;
+    [SerializeField] private bool isFullyLoad, canShoot;
 
+    void Start() 
+    {
+        anim = GetComponent<Animator>();
+
+    }
 
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.Mouse0) && Ammo >= 1)
+        if (Input.GetKeyDown(KeyCode.Mouse0) && Ammo >= 1 && isFullyLoad && canShoot)
         {
-            Debug.Log("Shoot");
-            Ammo -= 1;
-            Instantiate(Bullet, dulo.position, Quaternion.Euler(this.transform.right));
+            StartCoroutine(shoot());
+            Shoot();
+            canShoot = false;
         }
+            
+
         if (Input.GetKeyDown(KeyCode.R) && Ammo <= 0)
-            Ammo = Magazine;
+        {
+            Reload();
+            StartCoroutine(reload());
+            isFullyLoad = false;
+        }
+    }
+    void Shoot()
+    {
+        Transform BulletInstance = (Transform)Instantiate(Bullet.transform, dulo.position, Quaternion.identity);
+        BulletInstance.GetComponent<Rigidbody>().AddForce(-transform.right * force);
+        Ammo -= 1;
+        anim.SetTrigger("shooting");
+    }
+
+    void Reload()
+    {
+        Ammo = Magazine;
+        anim.SetTrigger("reloading");
+    }
+    private IEnumerator reload()
+    {
+        yield return new WaitForSeconds(reloadTime);
+        isFullyLoad = true;
+    }
+    private IEnumerator shoot()
+    {
+        yield return new WaitForSeconds(shootTime);
+        canShoot = true;
     }
 }
